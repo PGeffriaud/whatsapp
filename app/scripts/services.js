d52403cbdf22300d309d50feb78232eca0348970
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2016-02-12 10:27:29 +0100

    Add closures and 'use strict'

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,50 +1,54 @@
-angular.module('starter.services', [])
+(function () {
+  'use strict';
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  angular.module('starter.services', [])
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'images/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'images/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'images/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'images/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'images/mike.png'
-  }];
+    .factory('Chats', function () {
+      // Might use a resource here that returns a JSON array
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+      // Some fake testing data
+      var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'images/ben.png'
+      }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'images/max.png'
+      }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'images/adam.jpg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'images/perry.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'images/mike.png'
+      }];
+
+      return {
+        all: function () {
+          return chats;
+        },
+        remove: function (chat) {
+          chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+          for (var i = 0; i < chats.length; i++) {
+            if (chats[i].id === parseInt(chatId)) {
+              return chats[i];
+            }
+          }
+          return null;
         }
-      }
-      return null;
-    }
-  };
-});
+      };
+    });
+})();
